404eb33f4024bc94ab66c20724ef1850c10c3062
nocoolyoyo <574857444@qq.com>
2019-11-19 10:00:09 +0800

    修改tsconfig输出格式 LF

--- bin/config.js
+++ bin/config.js
@@ -1,14 +1,14 @@
-"use strict";
+'use strict';
 var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+  return (mod && mod.__esModule) ? mod : { default: mod }
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-var path_1 = __importDefault(require("path"));
-var packageJson = require(path_1.default.resolve('package.json'));
+Object.defineProperty(exports, '__esModule', { value: true })
+var path_1 = __importDefault(require('path'))
+var packageJson = require(path_1.default.resolve('package.json'))
 var config = {
-    output: "mini_node_modules/"
-};
-if (packageJson["mini-npm-config"]) {
-    Object.assign(config, packageJson["mini-npm-config"]);
+  output: 'mini_node_modules/'
 }
-exports.default = config;
+if (packageJson['mini-npm-config']) {
+  Object.assign(config, packageJson['mini-npm-config'])
+}
+exports.default = config
